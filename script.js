ff0f290eb24e68bfde250e2258ff76081251ec0a
thepjos <josh.thephasdin@gmail.com>
2023-02-28 09:07:30 -0600

    thank you screen built and toggling correctly

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const rateStar = document.querySelectorAll('.eachStar')
+const tyContainer = document.querySelector('.thankyou-container')
+const mainContainer = document.querySelector('.container')
+const submitButton = document.querySelector('.submit')
+const finalRating = document.querySelector('.finalRating')
+
+finIdx = 0
 
 rateStar.forEach((star, idx) => {
     star.addEventListener('click', () => highlightStar(idx))
@@ -16,5 +22,14 @@ function highlightStar(idx) {
             star.classList.remove('active')
         }
     })
+    return finIdx = idx + 1
 }
+
+submitButton.addEventListener('click', () => {
+    mainContainer.classList.add('hidden')
+    tyContainer.classList.remove('hidden')
+    finalRating.innerText = (`You selected ${finIdx} out of 5`)
+})
+
+
 
